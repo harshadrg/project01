e2ae5a26b6233ab63f199292a15a4982adac23c3
harshadrg <harshadgurnule7+github@gmail.com>
2025-12-16 19:20:16 +0530

    feat: configure routing and tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import MainLayout from './components/layout/MainLayout.jsx'
+import {Home , AboutUs, Services, Contact} from './pages/index.jsx'
+
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index path='' element={<Home/>}/>
+      <Route path='about-us' element={<AboutUs/>}/>
+      <Route path='services' element={<Services/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
