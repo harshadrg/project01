a3f39c80f218f5fca9cf010a3a09845b462fc4cf
harshadrg <harshadgurnule7+github@gmail.com>
2025-12-17 11:50:43 +0530

    feat: replace placeholder with responsive header, add logo, center navbar

--- src/components/layout/HeaderLayout.jsx
+++ src/components/layout/HeaderLayout.jsx
@@ -1,11 +1,24 @@
 import Navbar from "./Navbar";
+import logo from '/nextIn01.svg'
 
 function HeaderLayout() {
     return (
         <>
-            <h1>Header Layout</h1>
-            <Navbar/>
+            <header className="relative h-16 flex items-center px-11 bg-white/90 backdrop-blur shadow-sm border-b border-gray-200">
+                <div className="shrink-0 ">
+                    <img
+                        src={logo}
+                        alt="nextIn Logo"
+                        className="h-12 w-12"
+                    />
+                </div>
+                <div className="absolute right-4 
+                    md:left-1/2 md:right-auto md:-translate-x-1/2"
+                >
+                    <Navbar />
+                </div>
+            </header>
         </>
     )
 }
-export default HeaderLayout;
+export default HeaderLayout; 
