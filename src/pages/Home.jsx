9f45843bb75cbb6659fbe98f96001daab2147f21
harshadrg <harshadgurnule7+github@gmail.com>
2026-01-12 15:08:16 +0530

    feat: replace Home placeholder with full homepage sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-function Home(){
-    return(
+import Hero from "../components/features/homePage/Hero.jsx";
+import TechStack from "../components/features/homePage/TechStack.jsx";
+import ProblemSolution from "../components/features/homePage/ProblemSoultion.jsx";
+import CoreServices from "../components/features/homePage/CoreServices.jsx";
+import FounderMessage from "../components/features/homePage/FounderMessage.jsx";
+
+function Home() {
+    return (
         <>
-            <h1>Home Page</h1>
+            <Hero />
+            <TechStack/>
+            <ProblemSolution/>
+            <CoreServices/> 
+            <FounderMessage/>
         </>
     )
 }
