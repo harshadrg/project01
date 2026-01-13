3196bb36594d3d3bf4b25952ac19e4c1782723da
harshadrg <harshadgurnule7+github@gmail.com>
2026-01-13 17:16:41 +0530

    feat: switch to HashRouter for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+// import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import MainLayout from './components/layout/MainLayout.jsx'
 import {Home , AboutUs, Services, Contact} from './pages/index.jsx'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 
-const router =createBrowserRouter(
-  createRoutesFromElements(
+const routes =(
     <Route path='/' element={<MainLayout/>}>
       <Route index path='' element={<Home/>}/>
       <Route path='about-us' element={<AboutUs/>}/>
@@ -16,10 +16,12 @@ const router =createBrowserRouter(
       <Route path='contact' element={<Contact/>}/>
     </Route>
   )
-)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>{routes}</Routes>
+    </HashRouter>
   </StrictMode>,
 )
